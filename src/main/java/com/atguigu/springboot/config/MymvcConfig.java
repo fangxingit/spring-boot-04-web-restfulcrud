bd8ed3573304e8089afba07cef6ec40690930ef4
fangxingit <fangxin@newstrength.cn>
2019-11-20 15:04:43 +0800

    update

--- src/main/java/com/atguigu/springboot/config/MymvcConfig.java
+++ src/main/java/com/atguigu/springboot/config/MymvcConfig.java
@@ -1,19 +1,22 @@
 package com.atguigu.springboot.config;
 
+import com.atguigu.springboot.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@EnableWebMvc 不要全面接管SpringMvc
+//@EnableWebMvc //不要全面接管SpringMvc
 @Controller
 public class MymvcConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry){
-//        registry.addViewController("/atguigu").setViewName("success");
-        registry.addViewController("atguigu").setViewName("success");
-    }
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry){
+////        registry.addViewController("/atguigu").setViewName("success");
+//        registry.addViewController("atguigu").setViewName("success");
+//    }
 
     //所有WebMvcConfigurerAdapter组件都会一起起作用
     @Bean   //将组件注入到Spring 容器中
@@ -26,5 +29,10 @@ public class MymvcConfig extends WebMvcConfigurerAdapter {
             }
         };
         return adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
